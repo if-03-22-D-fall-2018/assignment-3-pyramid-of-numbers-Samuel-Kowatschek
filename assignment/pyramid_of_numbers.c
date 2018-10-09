e065ab274640db7f68942afb34969dd9838784c4
Kowatschek Samuel <samuel@saintvis.com>
2018-10-09 20:42:29 +0200

    Added and finished Divide and copy_big_int function, tried to fix multiplication function

--- assignment/pyramid_of_numbers.c
+++ assignment/pyramid_of_numbers.c
@@ -15,6 +15,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+#include <math.h>
 
 /// The maximum number of digits allowed in a big int.
 #define MAX_DIGITS 80
@@ -87,8 +88,17 @@ int main(int argc, char *argv[])
 	print_big_int(&firstNumber);
 	printf("\n%d\n",len);
 	multiply(&firstNumber, 5, &result);
-
 	printf("\n\n");
+
+	print_big_int(&result);
+	divide(&firstNumber, 5, &result);
+	print_big_int(&result);
+	printf("\n");
+	print_big_int(&firstNumber);
+	print_big_int(&result);
+	printf("\n\n");
+	copy_big_int(&firstNumber, &result);
+	print_big_int(&firstNumber);
 	print_big_int(&result);
 	printf("end");
 	return 0;
@@ -100,7 +110,7 @@ int strtobig_int(const char *str, int len, struct BigInt *big_int){
 	for (size_t i = 0; i < len; i++) {
 		if(str[i]>='0'&&str[i]<='9'){
 			int temp=str[i]-'0';
-			big_int->the_int[i] = temp;
+			big_int->the_int[counter] = temp;
 			counter++;
 		}
 	}
@@ -108,7 +118,6 @@ int strtobig_int(const char *str, int len, struct BigInt *big_int){
 
     return counter;
 }
-
 
 void print_big_int(const struct BigInt *big_int){
 for (int i=0; i < big_int->digits_count; i++) {
@@ -119,38 +128,78 @@ printf("\n");
 
 void multiply(const struct BigInt *big_int, int factor, struct BigInt *big_result){
 	int overflowNumber=0;
-	int newOverflowNumber=0;																			//integer newOverflowNumber is the new overflowNumber, so it's like a little cache for the overflowNumber
+	int newOverflowNumber=0;
 	for (int i = big_int->digits_count-1 ; i >=0; i--) {		     //for loop which counts from the end of the integer to the beginning, like we'd do it in reallife
 		int tempResult=big_int->the_int[i]*factor;								//tempResult is the temporary Result
 
+
 		if (tempResult>9) {																		//here we check if the tempResult is bigger than 9, because if it is, we'd have to split the two parts (e.g. 1 and 4 for 14) and put 1 to the next
 				newOverflowNumber=tempResult/10;
-				if(i==0&&newOverflowNumber>0){									//This if checks if the result is bigger tan big_int
-                    big_result->the_int[i+1]=tempResult%10+overflowNumber;	//if it is bigger, the "normal" number should be on position i+1, so more right, and the newOverflowNumber should be on pos i
-                    big_result->digits_count=(big_int->digits_count) +1;   //Also the length of the result would be one larger than the length of the big_int
-										big_result->the_int[i]=newOverflowNumber;
-				}else if(i==0){																							//if i == 0 but we don't have a new overflowNumber, the big_result int should just be tempResult+overFlowNumber
-				    big_result->the_int[i]=tempResult%10+overflowNumber;
-            big_result->digits_count=big_int->digits_count;				//also the length would be just as big as the length of the big_int
-				}else{
-					big_result->the_int[i]=tempResult+overflowNumber;				//this is basically the default case
-				}
-				overflowNumber=newOverflowNumber;
-		}else{
+				if(i==0&&tempResult+overflowNumber>9){
+                    big_result->the_int[i+1]=tempResult%10+overflowNumber;
 
-			if(i==0&&overflowNumber>0){																								//integer newOverflowNumber is the new overflowNumber, so it's like a little cache for the overflowNumber
-                    big_result->the_int[i+1]=tempResult+overflowNumber;				//here we dont need the modulu 10 next  to the tempresult because it only has one digit.
-                    big_result->digits_count=(big_int->digits_count) +1;			//length would be big_int len +1
-                    big_result->the_int[i]=overflowNumber;										//the_int[i] is only overFlowNumber, not newOverflowNumber, because there is no newOverflowNumber
+                    big_result->digits_count=(big_int->digits_count) +1;
+
+                    big_result->the_int[i]=newOverflowNumber;
 				}else if(i==0){
+
+				    big_result->the_int[i]=tempResult%10+overflowNumber;
+				    big_result->digits_count=big_int->digits_count;
+				    for(int i = big_int->digits_count-1; i >0 ;i--){
+
+                        big_result->the_int[i]=big_result->the_int[i-1];
+				    }
+				}else{
+				    big_result->the_int[i]=(tempResult+overflowNumber)%10;
+
+				}
+        overflowNumber=newOverflowNumber;
+		}
+		else{
+
+			if(i==0&&overflowNumber+tempResult>9){
+                    big_result->the_int[i+1]=(tempResult+overflowNumber);
+                    big_result->digits_count=(big_int->digits_count) +1;
+                    big_result->the_int[i]=overflowNumber;
+				}else if(i==0){
+
 				    big_result->the_int[i]=tempResult+overflowNumber;
+
                     big_result->digits_count=big_int->digits_count;
 				}else{
-				big_result->the_int[i]=tempResult+overflowNumber;
+
+                    big_result->the_int[i]=tempResult+overflowNumber;
 				}
+				overflowNumber=0;
 		}
 		newOverflowNumber=0;
 
 	}
 
+}
+
+void divide(const struct BigInt *big_int, int divisor, struct BigInt *big_result){
+    int overflowNumber=0;
+    int temp;
+    int counter=0;
+    bool alreadyNumberSetted=false;
+    for(int i=0;i<big_int->digits_count;i++){
+        temp=(big_int->the_int[i]+overflowNumber)/divisor;
+        if(temp==0&&alreadyNumberSetted){
+
+            big_result->the_int[counter]=(big_int->the_int[i]+overflowNumber)/divisor;
+            counter++;
+        }else if(temp!=0){
+         alreadyNumberSetted=true;
+         big_result->the_int[counter]=(big_int->the_int[i]+overflowNumber)/divisor;
+         counter++;
+        }
+        overflowNumber=big_int->the_int[i]%divisor;
+        overflowNumber*=10;
+    }
+    big_result->digits_count=counter;
+}
+
+void copy_big_int(const struct BigInt *from, struct BigInt *to){
+    *to=*from;
 }
